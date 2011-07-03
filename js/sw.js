bf9509ab2a86b34c2735872e2edccbf95ee0519b
wulff <wulff@ratatosk.net>
2011-07-03 19:05:55 +0200

    added navigation images, update js to show/hide links

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,29 @@
+// attach the cycle plugin to the slideshow div
 $(function() {
   $('#fade').cycle({
     prev: '#prev',
     next: '#next',
-    timeout: 0
+    timeout: 0,
+    before: onBefore
   });
+
+  $('#prev').hide();
 });
+
+// show and hide the prev/next links
+function onBefore() {
+  switch ($(this).attr('id')) {
+    case 'slide-1':
+      $('#prev').hide();
+      $('#next').show();
+      break;
+    case 'slide-2':
+      $('#prev').show();
+      $('#next').show();
+      break;
+    case 'slide-3':
+      $('#prev').show();
+      $('#next').hide();
+      break;
+  }
+}
